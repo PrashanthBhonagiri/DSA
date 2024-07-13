f6d241b40ea4b8958b975947e70b8721aeee76a2
bhonagp <bhonagp@amazon.com>
2024-07-13 11:09:56 +0530

    :white_check_mark: UVA01124 CPP

---
+++ UVA01124.cpp
@@ -0,0 +1,10 @@
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string line;
+    while(getline(cin, line)) {
+        cout<<line<<"\n";
+    }
+    return 0;
+}
